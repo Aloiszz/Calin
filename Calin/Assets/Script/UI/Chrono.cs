be73a7c3451ef689c8de9c916d0cab79d7745731
DUBOIS Kévin <k.dubois@rubika-edu.com>
2022-09-27 14:11:59 +0200

    Menu pause + des morceaux d'UI

--- Calin/Assets/Script/UI/Chrono.cs
+++ Calin/Assets/Script/UI/Chrono.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Chrono : MonoBehaviour
 {
-    public float chronometre;
+    public float timer;
+    private float seconds;
+    private float minutes;
+    private float hours;
+
+    [SerializeField] private Text stopWatchText;
     void Start()
     {
-        chronometre = 10;
+        timer = 0;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        chronometre = chronometre - Time.deltaTime;
-        Debug.Log(chronometre);
+        
+    }
+
+    void StopWatchCalcul()
+    {
+        timer += Time.deltaTime;
+        seconds = timer % 60;
+        minutes = timer / 60;
+        hours = timer / 3600;
+
+        stopWatchText.text = hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
     }
 }
